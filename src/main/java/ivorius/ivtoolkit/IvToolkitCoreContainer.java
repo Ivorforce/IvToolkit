c304b53c17e748205005254b2a30f6e47333e7c6
Ivorius <lukastenbrink@googlemail.com>
2014-09-30 22:37:13 +0200

    Convert IvToolkit to a CoreMod so it gets loaded along with other coremods

---
+++ src/main/java/ivorius/ivtoolkit/IvToolkitCoreContainer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2014 Lukas Tenbrink
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ivorius.ivtoolkit;
+
+import com.google.common.eventbus.EventBus;
+import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.ModMetadata;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+/**
+ * Created by lukas on 25.02.14.
+ */
+public class IvToolkitCoreContainer extends DummyModContainer
+{
+    public static Logger logger = LogManager.getLogger("ivtoolkit");
+
+    public IvToolkitCoreContainer()
+    {
+        super(new ModMetadata());
+
+        ModMetadata myMeta = super.getMetadata();
+        myMeta.authorList = Arrays.asList("Ivorius");
+        myMeta.description = "Uncategorized framework";
+        myMeta.modId = "ivtoolkit";
+        myMeta.version = "1.0";
+        myMeta.name = IvToolkitLoadingPlugin.NAME;
+        myMeta.url = "http://www.minecraftforum.net/topic/563257-172";
+    }
+
+    @Override
+    public boolean registerBus(EventBus bus, LoadController controller)
+    {
+        bus.register(this);
+        return true;
+    }
+}
---
+++ src/main/java/ivorius/ivtoolkit/IvToolkitLoadingPlugin.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014 Lukas Tenbrink
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ivorius.ivtoolkit;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+/**
+ * Created by lukas on 21.02.14.
+ */
+@IFMLLoadingPlugin.Name(value = IvToolkitLoadingPlugin.NAME)
+@IFMLLoadingPlugin.TransformerExclusions(value = {"ivorius.ivtoolkit.asm."})
+public class IvToolkitLoadingPlugin implements IFMLLoadingPlugin
+{
+    public static final String NAME = "IvToolkit";
+
+    @Override
+    public String[] getASMTransformerClass()
+    {
+        return new String[]{};
+    }
+
+    @Override
+    public String getModContainerClass()
+    {
+        return "ivorius.ivtoolkit.IvToolkitCoreContainer";
+    }
+
+    @Override
+    public String getSetupClass()
+    {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data)
+    {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass()
+    {
+        return null;
+    }
+}
