dd30d3fa00a8a4132a3f21a6d6d3b45b3024061b
Ivorius <lukastenbrink@googlemail.com>
2014-11-12 21:45:52 +0100

    Add IvItemRendererModelCustom

--- src/main/java/ivorius/ivtoolkit/items/IvItemRendererModelCustom.java
+++ src/main/java/ivorius/ivtoolkit/items/IvItemRendererModelCustom.java
@@ -21,12 +21,13 @@ import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.model.IModelCustom;
 import org.lwjgl.opengl.GL11;
 
 /**
- * Created by lukas on 18.04.14.
+ * Created by lukas on 12.11.14.
  */
-public class IvItemRendererModel implements IItemRenderer
+public class IvItemRendererModelCustom implements IItemRenderer
 {
     public ItemModelRenderer model;
     public ResourceLocation texture;
@@ -34,7 +35,7 @@ public class IvItemRendererModel implements IItemRenderer
     public float[] translation;
     public float[] rotation;
 
-    public IvItemRendererModel(ItemModelRenderer model, ResourceLocation texture, float modelSize, float[] translation, float[] rotation)
+    public IvItemRendererModelCustom(ItemModelRenderer model, ResourceLocation texture, float modelSize, float[] translation, float[] rotation)
     {
         this.model = model;
         this.texture = texture;
@@ -44,38 +45,37 @@ public class IvItemRendererModel implements IItemRenderer
     }
 
     @Override
-    public boolean handleRenderType(ItemStack item, ItemRenderType type)
+    public boolean handleRenderType(ItemStack item, IItemRenderer.ItemRenderType type)
     {
         return true;
     }
 
     @Override
-    public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper)
+    public boolean shouldUseRenderHelper(IItemRenderer.ItemRenderType type, ItemStack item, IItemRenderer.ItemRendererHelper helper)
     {
         return true;
     }
 
     @Override
-    public void renderItem(ItemRenderType type, ItemStack item, Object... data)
+    public void renderItem(IItemRenderer.ItemRenderType type, ItemStack item, Object... data)
     {
         GL11.glPushMatrix();
 
-        if (type == ItemRenderType.ENTITY)
+        if (type == IItemRenderer.ItemRenderType.ENTITY)
             GL11.glTranslated(0.0, 1.0, 0.0);
-        else if (type == ItemRenderType.INVENTORY)
+        else if (type == IItemRenderer.ItemRenderType.INVENTORY)
             GL11.glTranslated(0.0, 0.3, 0.0);
         else
             GL11.glTranslated(0.5, 1.0, 0.5);
 
         GL11.glTranslatef(translation[0], translation[1] + 1.0f, translation[2]);
 
-        if (type != ItemRenderType.ENTITY)
+        if (type != IItemRenderer.ItemRenderType.ENTITY)
         {
             float modelScale = 1.0f / modelSize;
             GL11.glScalef(modelScale, modelScale, modelScale);
         }
 
-        GL11.glRotatef(180.0f, 0.0f, 0.0f, 1.0f);
         GL11.glRotatef(rotation[0], 1.0f, 0.0f, 0.0f);
         GL11.glRotatef(rotation[1], 0.0f, 1.0f, 0.0f);
         GL11.glRotatef(rotation[2], 0.0f, 0.0f, 1.0f);
@@ -92,5 +92,21 @@ public class IvItemRendererModel implements IItemRenderer
     public static interface ItemModelRenderer
     {
         void render(ItemStack stack);
+    }
+
+    public static class ItemModelRendererSimple implements ItemModelRenderer
+    {
+        public IModelCustom model;
+
+        public ItemModelRendererSimple(IModelCustom model)
+        {
+            this.model = model;
+        }
+
+        @Override
+        public void render(ItemStack stack)
+        {
+            model.renderAll();
+        }
     }
 }
