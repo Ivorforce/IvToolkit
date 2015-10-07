61024001e58095baed5947e133c5de4e6654fc88
Ivorius <lukastenbrink@googlemail.com>
2015-10-07 13:09:42 +0200

    Bump version to 1.2.1.

--- src/main/java/ivorius/ivtoolkit/IvToolkitLoadingPlugin.java
+++ src/main/java/ivorius/ivtoolkit/IvToolkitLoadingPlugin.java
@@ -28,7 +28,7 @@ import java.util.Map;
 public class IvToolkitLoadingPlugin implements IFMLLoadingPlugin
 {
     public static final String NAME = "IvToolkit";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
     public static final String MODID = "ivtoolkit";
 
     @Override
