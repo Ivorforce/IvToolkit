e72a8e3c2bcdda45260875debba0ede70a0ad514
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-01-26 18:03:10 +0100

    Bump version to 1.3.2.

--- src/main/java/ivorius/ivtoolkit/IvToolkit.java
+++ src/main/java/ivorius/ivtoolkit/IvToolkit.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.Logger;
 public class IvToolkit
 {
     public static final String NAME = "IvToolkit";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     public static final String MODID = "ivtoolkit";
 
     public static Logger logger = LogManager.getLogger(MODID);
